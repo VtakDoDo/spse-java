fcae7fa0ab7ccaa5d479a906911adb07e4f741ba
Dominik Spišák <spisakkdominik17@gmail.com>
2023-10-23 19:44:39 +0200

    ide to či ako ?

--- prva.java
+++ prva.java
@@ -3,12 +3,10 @@ import java.util.Random;
 class A {
     public static void main(String[] args) {
         Random rand = new Random();
-        
-        int pocetPlodin = rand.nextInt(100) + 1; // Generovanie počtu plodín od 1 do 100
+        int pocetPlodin = rand.nextInt(100) + 1; 
         String[] typyPlodin = {"ovocie", "zelenina", "obilnina"};
-        String typPlodiny = typyPlodin[rand.nextInt(typyPlodin.length)]; // Náhodný výber typu plodiny
-        double urodaNaJednuPlodinu = rand.nextDouble() * 100; // Generovanie úrody od 0 do 100 kg
-        
+        String typPlodiny = typyPlodin[rand.nextInt(typyPlodin.length)]; 
+        double urodaNaJednuPlodinu = rand.nextDouble() * 100; 
         System.out.println("Počet plodín: " + pocetPlodin);
         System.out.println("Typ plodiny: " + typPlodiny);
         System.out.printf("Úroda na jednu plodinu: %.2f kg%n", urodaNaJednuPlodinu);
