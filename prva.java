9f8e2de694d1a2eeba52ae78614dc2e06df22882
Dominik Spišák <spisakkdominik17@gmail.com>
2023-10-23 18:57:42 +0200

    farma domaca

--- prva.java
+++ prva.java
@@ -1,7 +1,16 @@
+import java.util.Random;
+
 class A {
- public static void main(String args[]){
-    String meno = "Dominik";
-    System.out.println("Ahoj toto naprogramoval");
-    System.out.println(meno);
- }
+    public static void main(String[] args) {
+        Random rand = new Random();
+        
+        int pocetPlodin = rand.nextInt(100) + 1; // Generovanie počtu plodín od 1 do 100
+        String[] typyPlodin = {"ovocie", "zelenina", "obilnina"};
+        String typPlodiny = typyPlodin[rand.nextInt(typyPlodin.length)]; // Náhodný výber typu plodiny
+        double urodaNaJednuPlodinu = rand.nextDouble() * 100; // Generovanie úrody od 0 do 100 kg
+        
+        System.out.println("Počet plodín: " + pocetPlodin);
+        System.out.println("Typ plodiny: " + typPlodiny);
+        System.out.printf("Úroda na jednu plodinu: %.2f kg%n", urodaNaJednuPlodinu);
+    }
 }
